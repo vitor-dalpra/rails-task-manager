d6558fbb541635fcb07e213eae15d984fe83688f
Vitor Dal Pra <vitordalpra@gmail.com>
2020-11-17 14:24:08 -0300

    added some pages

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,25 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completed)
+  end
 end
